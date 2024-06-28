7270d0318201ac7c7b86fc1ba705fc6e11cdfa8a
Alnooboo <101204241+Alnooboo@users.noreply.github.com>
2024-06-28 15:56:35 +0300

    Add files via upload

---
+++ ts-01-first-example-basics/ts-01-first-example-basics/app.ts
@@ -0,0 +1,123 @@
+//// 1
+// class User {
+//   //we need to define the types of the variable that we will add
+//   name: string;
+//   private age: number;//access t oit only from inside the class
+
+//   constructor(name: string, age: number) {
+//     this.name = name;
+//     this.age = age;
+//   }
+// }
+
+// const user=new User("habeeb",20);
+// console.log(user.name, user.age);//age is not acces sible
+
+//we can use it to foce classes to have certain property or methods!
+interface Greetable {
+  name: string;
+}
+
+//2
+class User implements Greetable {
+  //now we cant delete the name because Greetable has it
+  constructor(public name: string, private age: number) {} //this is similar to the above one but shorter!
+}
+
+class Admin extends User {
+  //thats how to extends a class with TS while keeping the shorcut:
+  constructor(name: string, age: number, private permissions: string[]) {
+    super(name, age);
+  }
+}
+
+const user = new User('habeeb', 20);
+console.log(user.name);
+
+const num1Input = document.getElementById('num1') as HTMLInputElement;
+const num2Input = <HTMLInputElement>document.getElementById('num2');
+const buttonElement = document.querySelector('button') as HTMLInputElement;
+
+function add(a: number, b: number) {
+  return a + b;
+}
+
+//// 1
+// function printResult(result: string | number, printMode: 'console' | 'alert') {
+//   //the type that I'm allowing (could be also specific text)
+//   if (printMode === 'console') {
+//     console.log(result);
+//   } else if (printMode === 'alert') {
+//     alert(result);
+//   }
+// }
+
+//2
+enum OutputMode { //create an array of types
+  CONSOLE,
+  ALERT,
+}
+
+function printResult(result: string | number, printMode: OutputMode) {
+  //the type that I'm allowing (could be also specific text)
+  if (printMode === OutputMode.CONSOLE) {
+    console.log(result);
+  } else if (printMode === OutputMode.ALERT) {
+    alert(result);
+  }
+}
+
+//interface is similar to class but when defining no "new", and we can only initialize the methods and variables
+interface CalculationContainer {
+  res: number;
+  print(): void;
+}
+// type Calculations = CalculationContainer[]; //thats how we use it
+
+// new CalculationContainer//error
+
+// type Calculations = { res: number; print: () => void }[]; //define our own type
+
+// 1
+const results: { res: number; print: () => void }[] = []; //telling that I'm making an array of objects that hass these two elements
+
+// 2
+// const results: Calculations = [];//using generic
+
+//generic usage:
+//1
+// const results: Array<any> = [];//using
+
+//2
+// const results: Array<CalculationContainer> = [];//using generic
+
+buttonElement?.addEventListener('click', () => {
+  const num1 = +num1Input.value;
+  const num2 = +num2Input.value;
+  const result = add(num1, num2);
+
+  const resultContainer = {
+    res: result,
+    print() {
+      console.log(this.res);
+    },
+  };
+
+  // printResult(resultContainer.res);
+  results.push(resultContainer);
+  // printResult(result);
+  // results[0].print();
+
+  printResult(result, OutputMode.CONSOLE);
+  printResult(result, OutputMode.ALERT);
+  // printResult(result,"window");//will give an error
+});
+
+//create your own generic:
+
+function logAndEcho<T>(val: T) {//telling that the function is going to get a certain type, and the function itself returns a type
+  console.log(val);
+  return val;
+}
+
+logAndEcho<string>('hi there').split(' ');//now we defined the function type, which allows us to use the type's methods!
